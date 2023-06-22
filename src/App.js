9f1b1c0f35f71d39fc4027d4f6a177adf7fbea8f
Cem Tosun <134544650+Kilopi3142@users.noreply.github.com>
2023-06-22 19:18:27 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,215 @@
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+
+    <div>
+
+    <h1>Kilopi Roadmap</h1>
+
+    <section id="timeline">
+
+    
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2019 - 2021</p>
+        </div>
+
+        <div className="tl-content">
+
+<p className="finished">
+2019, The era of shaping the idea.
+</p>
+<p className="finished">
+2020, Research era for the possibility of being a real project with the idea.
+</p>
+<p className="finished">
+2021, Choosing the right blockchain for the needs of the project and testing this blockchain by deploying test dApps
+</p>
+
+        </div>
+      </div>
+
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2022</p>
+        </div>
+
+        <div className="tl-content">
+          <p className="finished">
+          Beginning era of the project. Creation of the token, forming the team, enlarging the idea to an updated version and adding more areas to the project.
+          </p>
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2023 <span className="q">Q1</span></p>
+        </div>
+
+        <div className="tl-content">
+          <p className="finished">
+          Masters of Management Alpha 3
+          </p>
+          <p className="finished">
+Masters of Management Alpha 4
+</p>
+<p className="finished">
+Staking system
+</p>
+<p className="finished">
+Tokenomics model
+</p>
+<p className="finished">
+Tokenomics Contracts
+</p>
+<p>
+Education Content
+</p>
+<p className="finished">
+Skallia Alpha 0
+</p>
+          
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2023 <span className="q">Q2</span></p>
+        </div>
+
+        <div className="tl-content">
+        <p>
+        Masters of Management Alpha 5
+        </p>
+<p>
+Masters of Management Alpha 6
+</p>
+<p>
+SES and Matcher Web
+</p>
+<p className="finished">
+Tokenomics Contracts
+</p>
+<p>
+Education Package 1
+</p>
+<p className="finished">
+Skallia Alpha 1
+</p>
+<p>
+Roadmap Check Meeting
+</p>
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2023 <span className="q">Q3</span></p>
+        </div>
+
+        <div className="tl-content">
+
+        <p>
+        Masters of Management Alpha 7
+        </p>
+<p>
+Masters of Management Alpha 8
+</p>
+<p className="finished">
+Pathfinder Mobile
+</p>
+<p>
+Staking Mobile
+</p>
+<p>
+Education Package 2
+</p>
+<p>
+Skallia Alpha 2
+</p>
+<p>
+New Poll System
+</p>
+<p>
+Skallia Alpha 3
+</p>
+
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2023 <span className="q">Q4</span></p>
+        </div>
+
+        <div className="tl-content">
+          <p>
+          Masters of Management Alpha 9
+          </p>
+<p>
+Masters of Management Alpha 10
+</p>
+<p>
+Education Package 3
+</p>
+<p>
+Skallia Alpha 4
+</p>
+<p>
+Roadmap Check Meeting
+          </p>
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">2024</p>
+        </div>
+
+        <div className="tl-content">
+          <p>
+            This section will be filled following the meeting in July 2023 about the developments that have been happened in 2023 and the roadmap decisions of 2024.
+          </p>
+        </div>
+      </div>
+
+      <div className="tl-item">
+        <div className="tl-bg"></div>
+
+        <div className="tl-year">
+          <p className="f2 heading--sanSerif">Long Term</p>
+        </div>
+
+        <div className="tl-content">
+          <p>
+          This section will be filled following the meeting in July 2023 about the developments that have been happened in 2023 and the roadmap decisions of 2025 and beyond.
+          </p>
+        </div>
+      </div>
+    </section>
+    </div>
+  );
+}
+
+export default App;
